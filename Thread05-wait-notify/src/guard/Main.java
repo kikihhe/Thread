a474bba7ea85dbac4d4ae741caa977f8f48fa757
2382546457 <2382546457@qq.com>
2023-02-16 20:55:16 +0800

    GuardObject

---
+++ Thread05-wait-notify/src/guard/Main.java
@@ -0,0 +1,14 @@
+package guard;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2023-02-16 20:50
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = 10.0;
+        System.out.println(a == b);
+    }
+}
