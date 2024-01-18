000dab64daf62ee4f0bce61791690efe0ccc2f4e
2382546457 <2382546457@qq.com>
2024-01-18 13:47:58 +0800

    spi

---
+++ Thread06-Timer/src/main/java/org/xiaohe/Main.java
@@ -0,0 +1,12 @@
+package org.xiaohe;
+
+/**
+ * @author : 小何
+ * @Description :
+ * @date : 2024-01-18 13:46
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
